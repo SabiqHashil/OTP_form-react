06679cf38642717ba6c2fa58fbb2cd80ba01ce70
sabiqhashil <sabiqhashilkp786@gmail.com>
2023-03-28 12:28:31 +0530

    inital commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <form action="">
+      <h3>Please fill in the otp</h3>
+      <div>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default App;
